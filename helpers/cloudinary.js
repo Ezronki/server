871096214a7e63c16d1c0deb238612de7e05975c
Ezron <ezronkiprotich@gmail.com>
2025-03-22 22:21:59 +0300

    cloudinary

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -7,7 +7,6 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true, // Ensure URLs are generated with HTTPS
 });
 
 const storage = new multer.memoryStorage();
@@ -16,7 +15,7 @@ async function imageUploadUtil(file) {
   const result = await cloudinary.uploader.upload(file, {
     resource_type: "auto",
   });
-  return result.url; // This will now be an HTTPS URL
+  return result;
 }
 
 const upload = multer({ storage });
